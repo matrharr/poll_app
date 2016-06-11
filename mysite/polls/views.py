48b5ceb4a9ac789ca83a1553bb1e92c1f15b1ae7
Matt Harris <matrharr@gmail.com>
2016-06-11 10:58:16 -0700

    import generic

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
