1d2ae1bf8903cf82d8addd2ef8170e8b7c068a68
Matt Harris <matrharr@gmail.com>
2016-06-11 11:25:32 -0700

    add test

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+import datetime
 from django.db import models
 
 class Question(models.Model):
@@ -8,7 +9,7 @@ class Question(models.Model):
   def __str__(self):
     return self.question_text
 
-  def was_published_recently():
+  def was_published_recently(self):
     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
