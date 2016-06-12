84f04bd8b4089d6dcd2542bab67592b676d065c9
Matt Harris <matrharr@gmail.com>
2016-06-12 10:55:39 -0700

    add partial and working ajax

--- mysite/polls/static/polls/polls.js
+++ mysite/polls/static/polls/polls.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
-  $('a').onClick()
-})
+  showPollInfoEvent();
+});
 
 
-var showInfo = function(e){
-  e.preventDefault()
+var showPollInfoEvent = function(){
+  $("a").on('click', function(e){
+    e.preventDefault()
+    var that = this
+    var route = $(this).attr('href')
+    var id = $(that).attr('href').split('/')[2]
+    console.log($('.poll' + id).next().length)
 
+    if( $('.poll' + id).next().length > 0){
+      $('.poll' + id).next().remove()
+      console.log('success')
+    }else{
+        $.ajax({
+                url: route,
+                method: 'get'
+        }).done(function(response){
+                $(that).after(response)
+        })
+    }
+  })
 }
